e7bfe9a04089b12c2403922106062f7b4cb16dff
Amir Ziaei <amir.ziaei@zenitech.co.uk>
2024-03-13 21:11:59 +0200

    personalize

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,35 +1,13 @@
 import type { MetaFunction } from '@remix-run/cloudflare'
 
 export const meta: MetaFunction = () => {
-  return [
-    { title: 'New Remix App' },
-    {
-      name: 'description',
-      content: 'Welcome to Remix! Using Vite and Cloudflare!',
-    },
-  ]
+  return [{ title: 'Amir Ziaei' }]
 }
 
 export default function Index() {
   return (
     <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.8' }}>
-      <h1>Welcome to Remix (with Vite and Cloudflare)</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://developers.cloudflare.com/pages/framework-guides/deploy-a-remix-site/"
-            rel="noreferrer"
-          >
-            🚀 Cloudflare Pages Docs - Remix guide
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+      <h1>Welcome to my website!</h1>
     </div>
   )
 }
